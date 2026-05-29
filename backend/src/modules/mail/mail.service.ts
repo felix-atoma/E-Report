9b6b37682f403f4c98793e7eb9543693482b0746
felix-atoma <felixatoma2@gmail.com>
2026-05-29 13:21:08 +0000

    fix: use default import for @sendgrid/mail to fix setApiKey error

--- backend/src/modules/mail/mail.service.ts
+++ backend/src/modules/mail/mail.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as nodemailer from 'nodemailer';
-import * as sgMail from '@sendgrid/mail';
+import sgMail from '@sendgrid/mail';
 
 // Re-export config reference for the owner email default
 const OWNER_EMAIL_DEFAULT = 'atomafelix2@gmail.com';
